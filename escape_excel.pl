81b348c1ad7c8a19663ccb2e6d13a4c6c718c78a
Paul Stewart <pstew@users.noreply.github.com>
2016-08-31 16:11:57 -0400

    Sci. notation bugfix and BSD boilerplate

--- escape_excel.pl
+++ escape_excel.pl
@@ -1,5 +1,34 @@
 #!/usr/bin/perl -w
 
+# Copyright (c) 2016 Eric A. Welsh. All Rights Reserved.
+#
+# Escape Excel is distributed under the following BSD-style license:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+                         
+
 use Scalar::Util qw(looks_like_number);
 
 $date_abbrev_hash{'jan'} = 'january';
@@ -153,7 +182,7 @@ if ($num_files == 0 || $syntax_error_flag)
     printf STDERR "Syntax: escape_excel.pl [options] tab_delimited_input.txt [output.txt]\n";
     printf STDERR "   Options:\n";
     printf STDERR "      --no-dates   Do not escape text that looks like dates\n";
-    printf STDERR "      --no-sci     Do not escape > #E (ex: 12E4) or >11 digit integer parts\n";
+    printf STDERR "      --no-sci     Do not escape > #E (ex: 12E4) or >11 digit integers\n";
     printf STDERR "      --no-zeroes  Do not escape leading zeroes (ie. 012345)\n";
     printf STDERR "      --paranoid   Escape *ALL* non-numeric text\n";
     printf STDERR "                   WARNING -- Excel can take a LONG time to import\n";
@@ -267,10 +296,10 @@ while(defined($line=<INFILE>))
               $array[$i] = sprintf "=\"%s\"", $array[$i];
           }
 
-          # Escape scientific notation with >= 2 digits before the E,
+          # Escape integer scientific notation with >= 2 digits before the E,
           #  since they are likely accessions or plate/well identifiers.
           #
-          # Also escape numbers with >11 digits before the decimal point.
+          # Also escape integers with >11 digits.
           # >11 is when it displays scientific notation in General format,
           #  which can result in corruption when saved to text.
           # >15 would be the limit at which it loses precision internally.
@@ -287,7 +316,7 @@ while(defined($line=<INFILE>))
               $temp = $array[$i];
               $temp =~ s/\,//g;
               
-              if ($temp =~ /^([1-9][0-9]{11,}|[0-9]{2,}[eE])/)
+              if ($temp =~ /^[1-9][0-9]{11,}$|^[0-9]{2,}[eE]/)
               {
                   $array[$i] = sprintf "=\"%s\"", $array[$i];
               }
